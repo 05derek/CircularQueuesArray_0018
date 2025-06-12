8f69bd513d7cb2f2c529aebe3038d4c367d6bcb0
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-06-12 10:14:28 +0700

    jika rear berada diposisi terakhir rear

--- Queues.cpp
+++ Queues.cpp
@@ -1,17 +1,10 @@
 
-
-        // cek apakah antrian penuh
-        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-            cout << "\nQueue overflow\n";
-            return;
+        else {
+            //jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+            REAR = 0;
+            else
+            REAR = REAR + 1;
         }
-
-        //cek apakah antrian kosong
-        if (FRONT == -1) {
-            FRONT = 0;
-            REAR = 0
-            
-        }
-
-        
+        queue_array[REAR] = num;
 }
