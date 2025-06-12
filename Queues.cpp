beef39d6baada7e201e0808ecaf839796b2bf91c
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-06-12 10:31:33 +0700

    mengecek apakah antrian kosong

--- Queues.cpp
+++ Queues.cpp
@@ -1,6 +1,9 @@
 
 
-void display() {
-    int FRONT_position + FRONT;
-    int REAR_position = REAR;
+//cek apakah antrian kosong
+if (FRONT == 1) {
+    cout << "Queue is empety\n"
+    return;
 }
+
+cout << "\nElement in the queue are...\n";
