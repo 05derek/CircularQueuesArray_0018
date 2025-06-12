7728c1947f2fd6131fa6250c1def43d2f775fefd
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-06-12 10:07:03 +0700

    pembuatan void insert

--- Queues.cpp
+++ Queues.cpp
@@ -1,9 +1,10 @@
 
 
-    public:
-        Queues() {
-            FRONT = -1;
-            REAR = -1;
+        void insert() {
+            int num;
+            cout << "Enter a number: ";
+            cin >> num;
+            cout << endl;
         }
 
         
